c75b4e335a212931a61fd4921ac804683965339c
deadhand89 <bandeeka@gmail.com>
2021-09-28 13:28:09 +0200

    Create turtlegame.py
    pyth1

---
+++ turtlegame.py
@@ -0,0 +1,15 @@
+import turtle
+
+teknos = turtle.Turtle()
+teknos.speed(1)
+teknos.shape ("turtle")
+teknos.color ("green")
+
+teknos.forward (200)
+teknos.right (90)
+teknos.forward (200)
+teknos.right (90)
+teknos.forward (200)
+teknos.right (90)
+teknos.forward (200)
+teknos.right (90)
