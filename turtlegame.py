1d7d390239e3176ba8f597295372ae34f5abe31d
deadhand89 <bandeeka@gmail.com>
2021-09-28 14:21:22 +0200

    tektek

--- turtlegame.py
+++ turtlegame.py
@@ -1,9 +1,18 @@
 import turtle
 
+#window
+
+ablak = turtle.Screen()
+ablak.setup(width=800, height=600)
+ablak.bgcolor("black")
+ablak.title("TurtlePong")
+
 teknos = turtle.Turtle()
 teknos.speed(1)
 teknos.shape ("turtle")
 teknos.color ("green")
+teknos.shapesize (stretch_wid=5, stretch_len=5)
+teknos.penup()
 
 teknos.forward (200)
 teknos.right (90)
